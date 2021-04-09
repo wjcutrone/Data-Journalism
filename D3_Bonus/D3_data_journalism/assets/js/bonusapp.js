8e6c48c7434608de5a744ee7c9a5c7b20d7f0d99
wjcutrone <73838888+wjcutrone@users.noreply.github.com>
2021-04-09 14:40:33 -0400

    updates
    updates to readme

--- D3_Bonus/D3_data_journalism/assets/js/bonusapp.js
+++ D3_Bonus/D3_data_journalism/assets/js/bonusapp.js
@@ -108,12 +108,6 @@ d3.csv("assets/data/census.csv").then(function(censusData) {
     var xScale = setXScale(censusData, chosenXAxis, chartWidth);
     var yScale = setYScale(censusData, chosenYAxis, chartHeight);
 
-
-    //YScale Funcion
-    // var yScale = d3.scaleLinear()
-    //     .domain([0, d3.max(censusData, d => d.poverty)])
-    //     .range([chartHeight, 0]);
-
     //Create initial axes functions
     var bottomAxis = d3.axisBottom(xScale);
     var leftAxis = d3.axisLeft(yScale);
@@ -339,86 +333,3 @@ d3.csv("assets/data/census.csv").then(function(censusData) {
 
 
 
-// var chartGroup = svg.append("g")
-//     .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-// //Import data
-// d3.csv("assets/data/census.csv").then(function(censusData) {
-
-//     //Parse Data
-//     censusData.forEach(function(data) {
-//         data.obesity = +data.obesity;
-//         data.poverty = +data.poverty;
-//     });
-
-//     //Create Scales
-//     var xScale = d3.scaleLinear()
-//         .domain([0, d3.max(censusData, d => d.obesity)])
-//         .range([0, chartWidth]);
-    
-//     var yScale = d3.scaleLinear()
-//         .domain([0, d3.max(censusData, d => d.poverty)])
-//         .range([chartHeight,0]);
-    
-//     //Create Axes
-//     var bottomAxis = d3.axisBottom(xScale);
-//     var leftAxis = d3.axisLeft(yScale);
-
-//     //Append Axes to Chart
-//     chartGroup.append("g")
-//         .attr("transform", `translate(0, ${chartHeight})`)
-//         .call(bottomAxis);
-    
-//     chartGroup.append("g")
-//         .call(leftAxis);
-    
-//     //Create circles for data points
-//     var circlesGroup = chartGroup.selectAll("circle")
-//         .data(censusData)
-//         .enter()
-//         .append("circle")
-//         .attr("cx", d => xScale(d.obesity))
-//         .attr("cy", d => yScale(d.poverty))
-//         .attr("r", 18)
-//         .attr("fill", "green")
-//         .attr("opacity", "0.5")
-        
-//         //Enable text to be added to the circles
-//         var cicleLabels = chartGroup.selectAll(null).data(censusData)
-//             .enter()
-//             .append("text");
-        
-//         //Fill circles with the state names
-//         cicleLabels
-//             .attr("x", function(d) {
-//                 return xScale(d.obesity)
-//             })
-//             .attr("y", function(d) {
-//                 return yScale(d.poverty)
-//             })
-//             .text(function(d) {
-//                 return d.abbr;
-//             })
-//             .attr("font-family", "sans-serif")
-//             .attr("font-size", "10px")
-//             .attr("text-anchor", "middle")
-//             .attr("fill", "blue");
-
-//             //Axes Labels
-//             //y axis label
-//             chartGroup.append("text")
-//                 .attr("transform", "rotate(-90)")
-//                 .attr("y", 0 - margin.left + 40)
-//                 .attr("x", 0 - (chartHeight/2))
-//                 .attr("dy", "1em")
-//                 .attr("class", "axisText")
-//                 .text("Percent in Poverty")
-//             //x axis label
-//             chartGroup.append("text")
-//                 .attr("transform", `translate(${chartWidth/2}, ${chartHeight + margin.top + 30})`)
-//                 .attr("class", "axisText")
-//                 .text("Percent Obese");
-
-//         }).catch(function(error) {
-//             console.log(error);
-//         });
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Data-Journalism
-Utilization of D3, javascript and html to visualize census data
+The objective of this repository was to utilize D3.js to construct plots and put those plots on a webpage. The data used for the plot came from csv file that contained United States census data from the year 2014. D3 was used to read the csv and then make a scatter plot from the data. The two variables of interest in the scatter plot were percent obese per state and percent of people in poverty per state. From the appearance of the plot, there seemed to be a slight correlation between the two. The image of the resulting plot can be seen here. 
+
+![scatter.png](D3_data_journalism/assets/images/scatter.png)
